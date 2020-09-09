fc38c5ea129e2f177695e75419daa94e50e91431
Evgen-ios <jack38irk@icloud.com>
2020-09-09 21:01:46 +0800

    Initial Commit

---
+++ Allert/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Allert
+//
+//  Created by Евгений on 09.09.2020.
+//  Copyright © 2020 Евгений. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
